37724b5780f44261203c27b60eb3d025ed2f079c
Imran Mohamed <ibm786@icloud.com>
2021-05-20 16:11:37 +0100

    updated streak and reset

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const moment = require('moment')
 const simpleGit = require('simple-git')
 const random = require('random')
 const FILE_PATH = './data.json'
+
 const makeCommit = n =>{
     if(n===0) return simpleGit().push();
-    const x = random.int(0,54)
-    const y = random.int(0,6)
-    const DATE = moment().subtract(1,'y').add(x,'w').add(y,'d').format()
+    const y = random.int(0,131)
+    const DATE = moment().subtract(131,'d').add(y,'d').format()
     const data = {
         date : DATE
     }
@@ -18,4 +18,4 @@ const makeCommit = n =>{
     })
 }
 
-makeCommit(10)
+makeCommit(9)
